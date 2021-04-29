192c7dfae86a6d080d6ce1265391094383081cc7
Vivi <vivi@marsis.io>
2021-04-29 15:26:45 +0800

    [Add] add ts

--- src/abi/WyvernAtomicizer.ts
+++ src/abi/WyvernAtomicizer.ts
@@ -1,3 +1,24 @@
 import { PartialReadonlyContractAbi } from "../types"
 
-export const WyvernAtomicizer: PartialReadonlyContractAbi = [{"constant": false, "inputs": [{"name": "addrs", "type": "address[]"}, {"name": "values", "type": "uint256[]"}, {"name": "calldataLengths", "type": "uint256[]"}, {"name": "calldatas", "type": "bytes"}], "name": "atomicize", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"}]
+export const WyvernAtomicizer: PartialReadonlyContractAbi =
+  [{
+    "constant": false,
+    "inputs": [{
+      "name": "addrs",
+      "type": "address[]"
+    }, {
+      "name": "values",
+      "type": "uint256[]"
+    }, {
+      "name": "calldataLengths",
+      "type": "uint256[]"
+    }, {
+      "name": "calldatas",
+      "type": "bytes"
+    }],
+    "name": "atomicize",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }]
